4201afcfc35d622cb24f168b7f57a476f04de02d
Jaehyun Yoon <yuchem2@gmail.com>
2026-05-06 18:09:31 +0900

    RENWAL-TF-56 페이지 이동 시 스크롤 위치 초기화

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,10 +2,21 @@ import { Navigation } from '@/components/navigation'
 import { Footer } from '@/components/footer'
 import { AnimatePresence, motion } from 'framer-motion'
 import { useLocation, useOutlet } from 'react-router-dom'
+import { useEffect } from 'react'
 
 export default function Layout() {
   const location = useLocation()
   const outlet = useOutlet()
+
+  useEffect(() => {
+    if ('scrollRestoration' in window.history) {
+      window.history.scrollRestoration = 'manual'
+    }
+  }, [])
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' })
+  }, [location.pathname])
 
   return (
     <div className="font-sans antialiased min-h-screen flex flex-col">
